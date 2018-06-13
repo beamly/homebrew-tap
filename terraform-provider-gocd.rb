8fd959721ce1f3cb826d372ca0505467fb0d94c5
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-13 11:49:59 +0100

    terraform-provider-gocd version 0.1.21

--- terraform-provider-gocd.rb
+++ terraform-provider-gocd.rb
@@ -1,9 +1,9 @@
 class TerraformProviderGocd < Formula
   desc "Terraform Provider for ThoughtWorks GoCD"
   homepage "https://github.com/beamly/terraform-provider-gocd"
-  url "https://github.com/beamly/terraform-provider-gocd/releases/download/0.1.20/terraform-provider-gocd-0.1.20-darwin-amd64.zip"
-  version "0.1.20"
-  sha256 "7ca4d9a1b882b7f642eece99bdd13478c12af10854d2b0049e2c5f77167f5680"
+  url "https://github.com/beamly/terraform-provider-gocd/releases/download/0.1.21/terraform-provider-gocd-0.1.21-darwin-amd64.zip"
+  version "0.1.21"
+  sha256 "f5a0202c6954536cfb275c85c16c781eccbb8be5c69f69bad10e5cec7427884b"
   
   depends_on "terraform"
   depends_on "drewsonne/homebrew-tap/tf-install-provider"
