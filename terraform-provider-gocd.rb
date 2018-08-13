b677f00246d6610ac2c88aa23ed0870e8522895d
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-13 19:20:40 +0100

    Brew formula update for terraform-provider-gocd version 0.1.22

--- terraform-provider-gocd.rb
+++ terraform-provider-gocd.rb
@@ -1,9 +1,9 @@
 class TerraformProviderGocd < Formula
   desc "Terraform Provider for ThoughtWorks GoCD"
   homepage "https://github.com/beamly/terraform-provider-gocd"
-  url "https://github.com/beamly/terraform-provider-gocd/releases/download/0.1.21/terraform-provider-gocd-0.1.21-darwin-amd64.zip"
-  version "0.1.21"
-  sha256 "f5a0202c6954536cfb275c85c16c781eccbb8be5c69f69bad10e5cec7427884b"
+  url "https://github.com/beamly/terraform-provider-gocd/releases/download/0.1.22/terraform-provider-gocd-0.1.22-darwin-amd64.zip"
+  version "0.1.22"
+  sha256 "3eafbb1d029ea22ec41ced1bc6c3b07fee2c96881cbb295242833287ec208312"
   
   depends_on "terraform"
   depends_on "drewsonne/homebrew-tap/tf-install-provider"
@@ -12,10 +12,10 @@ class TerraformProviderGocd < Formula
     bin.install "terraform-provider-gocd"
   end
 
-  def caveats
-    "After installing, you will need to copy the provider to a place terraform can find it by running:
-  tf-install-provider artifactory
-"
+  def caveats; <<~EOS
+    After installing, you will need to copy the provider to a place terraform can find it by running:
+      tf-install-provider artifactory
+  EOS
   end
 
   test do
