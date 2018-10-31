f4d122d8546e7cf422fe91d8761a6b9cee1aa002
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-31 14:21:10 +0000

    Brew formula update for go-gocd version 0.7.2

--- go-gocd.rb
+++ go-gocd.rb
@@ -1,9 +1,9 @@
 class GoGocd < Formula
   desc "CLI interface for ThoughtWorks GoCD"
   homepage "https://github.com/beamly/terraform-provider-gocd"
-  url "https://github.com/beamly/go-gocd/releases/download/0.7.1/gocd-0.7.1-darwin-x86_64.zip"
-  version "0.7.1"
-  sha256 "e68b11d0724f392b3f46a6f3b1b01396bb2737848097dac9d350af3310a82e53"
+  url "https://github.com/beamly/go-gocd/releases/download/0.7.2/gocd-0.7.2-darwin-x86_64.zip"
+  version "0.7.2"
+  sha256 "37058406371f88b3e463b35a1bee419902c46a62cde72096b11a1bf71f601376"
   
   depends_on "drewsonne/homebrew-tap/tf-install-provider"
 
