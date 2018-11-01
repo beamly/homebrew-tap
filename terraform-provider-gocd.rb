4deaed2929cb1a8b9c03d866991061b735953c41
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-01 11:43:00 +0000

    Brew formula update for terraform-provider-gocd version 0.1.24

--- terraform-provider-gocd.rb
+++ terraform-provider-gocd.rb
@@ -1,9 +1,9 @@
 class TerraformProviderGocd < Formula
   desc "Terraform Provider for ThoughtWorks GoCD"
   homepage "https://github.com/beamly/terraform-provider-gocd"
-  url "https://github.com/beamly/terraform-provider-gocd/releases/download/0.1.23/terraform-provider-gocd-0.1.23-darwin-amd64.zip"
-  version "0.1.23"
-  sha256 "aa59c485da7776e816c539459c62c03081db733d1e71b67fc074645a527777fb"
+  url "https://github.com/beamly/terraform-provider-gocd/releases/download/0.1.24/terraform-provider-gocd-0.1.24-darwin-amd64.zip"
+  version "0.1.24"
+  sha256 "c2b8cd426c2ba2cbe445d7042c4cc1f5c970014833419b096c879f92b32bc8f0"
   
   depends_on "terraform"
   depends_on "drewsonne/homebrew-tap/tf-install-provider"
